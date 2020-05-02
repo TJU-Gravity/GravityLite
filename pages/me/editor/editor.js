9c15c80a06953289eb02c92684984835bb763dfe
Rebecca-yh <1652818@tongji.edu.cn>
2020-05-02 16:08:19 +0800

    editor

--- pages/me/editor/editor.js
+++ pages/me/editor/editor.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    privacy:15,
+    wechatVisibility:'eye-o',
+    phonenumberVisibility:'eye-o',
+    emailVisibility:'eye-o',
+    QQVisibility:'eye-o'
   },
 
   /**
@@ -62,5 +66,70 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  changeVisibility: function(options) {
+    var index= options.currentTarget.dataset.index
+    console.log("index"+index)
+    if(index==1)
+    {
+      //wechat
+      if((this.data.privacy&1)==1){
+        
+        this.setData({
+          wechatVisibility:'closed-eye',
+          privacy:(~1)&this.data.privacy
+        })
+      }else{
+        this.setData({
+          wechatVisibility:'eye-o',
+          privacy:1|this.data.privacy
+        })
+      }
+     
+    }
+    if(index==2){
+     //phone
+     if((this.data.privacy&2)==2){
+      this.setData({
+        phonenumberVisibility:'closed-eye',
+        privacy:(~2)&this.data.privacy
+      })
+    }else{
+      this.setData({
+        phonenumberVisibility:'eye-o',
+        privacy:2|this.data.privacy
+      })
+    }
   }
+  if(index==3){
+     //emial
+     if((this.data.privacy&4)==4){
+      this.setData({
+        emailVisibility:'closed-eye',
+        privacy:(~4)&this.data.privacy
+      })
+    }else{
+      this.setData({
+        emailVisibility:'eye-o',
+        privacy:4|this.data.privacy
+      })
+    }}
+    if(index==4){
+      //qq
+      if((this.data.privacy&8)==8){
+       this.setData({
+         QQVisibility:'closed-eye',
+         privacy:(~8)&this.data.privacy
+       })
+     }else{
+       this.setData({
+         QQVisibility:'eye-o',
+         privacy:8|this.data.privacy
+       })
+     }}
+   
+    console.log("privacy"+this.data.privacy)
+  }
+  
 })
